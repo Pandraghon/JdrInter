b2e3805502d6a5387af857dc8a756f61b0b237c5
Sapiensy <ryuunushi@gmail.com>
2014-02-07 20:09:55 +0100

    Amélioration launcher

--- launch.bat
+++ launch.bat
@@ -1,7 +1,4 @@
+@echo off
+xcopy C:\wamp\www\JdrInter G:\Projets\JdrInter /d /e /i /h /y /exclude:C:\wamp\www\JdrInter\exclude.txt
 cd C:\wamp\www\JdrInter
-xcopy public G:\Projets\JdrInter\public /e /i /y /exclude:exclude.txt
-xcopy index.html G:\Projets\JdrInter /e /i /y /exclude:exclude.txt
-xcopy package.json G:\Projets\JdrInter /e /i /y /exclude:exclude.txt
-xcopy Procfile G:\Projets\JdrInter /e /i /y
-xcopy server.js G:\Projets\JdrInter /e /i /y /exclude:exclude.txt
 foreman start &
